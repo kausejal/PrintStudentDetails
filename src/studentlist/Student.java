bf721c3fc043d952018ffd1a5daef0098d29109e
kausejal <kausejal@sheridancollege.ca>
2025-01-25 19:21:51 -0500

    Update Student.java

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -13,7 +13,7 @@ public class Student // model the student raw data
     private String name; // variables private 
    private int sid; // principle "encapsulation"
 private int sem;
-    private int year;
+    private int year; //changes to check pull
     /**
      * @return the name
      */
